46a65dc0372ed8f63cfac0026c75c3c9d16ba0e1
HeidiDragomir <marinela.dragomir@chasacademy.se>
2021-11-24 11:03:03 +0100

    create first function to generate a random word

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
 // Globala variabler
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+// Array: med spelets alla ord
+const wordList = ["egenskap", "uppskattar", "omständighet"];
 
+// Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+let selectedWord = "";
+
+// DOM-nod: knappen som du startar spelet med
+let startGameBtn = document.querySelector("#startGameBtn");
+
+// Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBox = document.querySelector("#letterBoxes > ul");
+
+let maxWrong = 6;   //Number: 
 let guesses = 0;     // Number: håller antalet gissningar som gjorts
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
 let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
 let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+//startGameBtn.addEventListener("click", function startGame() {
+
+    // Funktion som slumpar fram ett ord
+    function generateRandomWord() {
+        selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+    }
+    generateRandomWord();
+
+
+     // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+    /*function createLetterBoxes() {
+        for(let i = 0; i < selectedWord.length; i++) {
+        selectedWord.push(i);
+
+     }
+console.log(selectedWord);
+
+    }*/
+
+//}
+
+//)
+
+
+
+
+
+
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
