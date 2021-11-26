f97287a86cd752b423eaae205c2fd1a8367cbfb3
HeidiDragomir <marinela.dragomir@chasacademy.se>
2021-11-26 08:53:36 +0100

    update createLetterBoxes function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
 // Globala variabler
 
-// Array: med spelets alla ord
-const wordList = ["egenskap", "uppskattar", "omständighet"];
 
-// Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
-let selectedWord = "";
-let guess;
+let startGameBtn = document.querySelector("#startGameBtn");     // DOM-nod: knappen som du startar spelet med
+const wordList = ["hey", "uppskattar", "helloo"];    // Array: med spelets alla ord
+let selectedWord;       // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+let itemList;
+let inputItem;
 let guesses = [ ];     // Number: håller antalet gissningar som gjorts
-// Array av DOM-noder: Rutorna där bokstäverna ska stå
-let letterBox;;
+let letterBox;         // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterButton = document.querySelectorAll("ul button"); // Array av DOM-noder: Knapparna för bokstäverna
 
-
-
-// DOM-nod: knappen som du startar spelet med
-let startGameBtn = document.querySelector("#startGameBtn");
-
-
+let lettersElement = null;
 
 let maxWrong = 6;   //Number: 
-
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
-
 let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
 
 
+/*
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-//startGameBtn.addEventListener("click", function startGame() {
+startGameBtn.addEventListener("click", function startGame() {*/
+
+
+
 
     // Funktion som slumpar fram ett ord
     function generateRandomWord() {
@@ -35,33 +31,59 @@ let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
     generateRandomWord();
     wordList.push(selectedWord);
     
-    
+
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
     function createLetterBoxes() {
         for (let i = 0; i < selectedWord.length; i++) {
             letterBox = document.querySelector("#letterBoxes > ul");
-            letterBox.setAttribute("id", "new-word");
-            guess = document.createElement("li");
-            guess.setAttribute("class", "guess");
-            guess.innerHTML = "_";
-            guesses.push(guess);
-            letterBox.appendChild(guess);
+            letterBox.setAttribute("id", "selected-word");
+            itemList = document.createElement("li");
+            letterBox.appendChild(itemList);
+            inputItem = document.createElement("input");
+            inputItem.setAttribute("type", "text");
+            inputItem.setAttribute("class", "letter");
+            inputItem.setAttribute("value", "");
+            itemList.appendChild(inputItem);
         }
     }
     createLetterBoxes();
-     
-     
-    
+   
 
-//}
+/*
+}
 
-//)
-
+)*/
 
 
 
 
 
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
+// Funktion som körs när du trycker på bokstäverna och gissar bokstav 
+
+
+letterButton.forEach(button => {
+    button.addEventListener("click", function guessWord() {
+       console.log(guesses.push(button.textContent));
+        
+    });
+});
+
+//letterButton.addEventListener("click", function guessWord() {
+   
+   
+  
+   /*if (selectedWord.includes(letter)) {
+       const indexesList = getindexes(letter);
+       indexesList.forEach((val, i) => {
+           letterButton[value].textContent = this.textContent;
+       });
+   }*/
+
+
+
+
+
+
+
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
